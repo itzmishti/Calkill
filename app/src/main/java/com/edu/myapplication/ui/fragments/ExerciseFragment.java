70b590181f971e76428cbda86d21610ad313f2de
akshat <ofakshat2001@gmail.com>
2021-07-30 23:20:11 +0530

    v2

--- app/src/main/java/com/edu/myapplication/ui/fragments/ExerciseFragment.java
+++ app/src/main/java/com/edu/myapplication/ui/fragments/ExerciseFragment.java
@@ -10,11 +10,7 @@ import android.view.ViewGroup;
 
 import com.edu.myapplication.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ExerciseFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class ExerciseFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
