b1625a08bcd2a68698bbfcf0f09deba791ad80f3
akshat <ofakshat2001@gmail.com>
2021-07-31 00:42:07 +0530

    v2.1

--- app/src/main/java/com/edu/myapplication/remote/Firebase/FirebaseUnloader.java
+++ app/src/main/java/com/edu/myapplication/remote/Firebase/FirebaseUnloader.java
@@ -1,4 +1,7 @@
 package com.edu.myapplication.remote.Firebase;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class FirebaseUnloader {
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
 }
